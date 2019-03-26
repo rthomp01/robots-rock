830e7988dc83356e32c254bc56207ebb14ea92af
Ryan <r.thomp09@gmail.com>
2019-03-25 22:32:46 -0700

    Controller Update
    +ControllerConfig - scriptable object for per instance data for character controller
    
    Smoothed raw keyboard input for movement and locomotion blending

---
+++ Robots Rock/Assets/Scripts/ControllerConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ControllerConfig", menuName = "Configuration/Controller")]
+public class ControllerConfig : ScriptableObject
+{
+    public float walkSpeed = 3.0f;
+    public float airborneSpeed = 1.75f;
+    public float turnRate = 720.0f;
+    public float inputDampening = 0.1f;
+
+}
--- Robots Rock/Assets/Scripts/RobotController.cs
+++ Robots Rock/Assets/Scripts/RobotController.cs
@@ -6,54 +6,48 @@ using UnityEngine;
 public class RobotController : MonoBehaviour
 {
     public Rigidbody Rigidbody { get; private set; }
-    public Vector2 InputVector { get; private set; }
+    public float HorizontalMove { get; set; }
 
     //set in inspector
     public Animator animator;
-    public float moveSpeed = 3.0f;
-    public float turnRate = 45.0f;
-    public float locomotionDampening = 0.1f;
+    public ControllerConfig config;
 
     //private
     private Quaternion goalRotation;
     private float inputVelocity;
+    private float xInput;
 
     private void Awake()
     {
         Rigidbody = GetComponent<Rigidbody>();
         goalRotation = transform.rotation;
-
-        if(animator == null)
-        {
-            Debug.LogFormat("Animator reference mission on {0}!", transform.name);
-            this.enabled = false;
-        }
     }
 
     private void Update()
     {
-        InputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        xInput = Input.GetAxisRaw("Horizontal");
+        HorizontalMove = Mathf.SmoothDamp(HorizontalMove, xInput, ref inputVelocity, config.inputDampening);
         UpdateAnimator();
     }
 
     private void FixedUpdate()
     {
-        Rigidbody.velocity = new Vector3(InputVector.x * moveSpeed, Rigidbody.velocity.y, Rigidbody.velocity.z);
+        Rigidbody.velocity = new Vector3(HorizontalMove * config.walkSpeed, Rigidbody.velocity.y, Rigidbody.velocity.z);
+        Debug.Log(Rigidbody.velocity);
         UpdateRotation();
     }
 
     void UpdateRotation()
     {
-        if(InputVector.x != 0.0f)
+        if (xInput != 0.0f)
         {
-            goalRotation = InputVector.x > 0.0f ? Quaternion.LookRotation(Vector3.right) : Quaternion.LookRotation(-Vector3.right);
+            goalRotation = xInput > 0.0f ? Quaternion.LookRotation(Vector3.right) : Quaternion.LookRotation(-Vector3.right);
         }
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRotation, turnRate * Time.deltaTime);
+        Rigidbody.rotation = Quaternion.RotateTowards(transform.rotation, goalRotation, config.turnRate * Time.deltaTime);
     }
 
     void UpdateAnimator()
     {
-        animator.SetFloat("WalkSpeed", Mathf.SmoothDamp(animator.GetFloat("WalkSpeed"), Mathf.Abs(InputVector.x),
-            ref inputVelocity, locomotionDampening));
+        animator.SetFloat("WalkSpeed", Mathf.Abs(HorizontalMove));
     }
 }
