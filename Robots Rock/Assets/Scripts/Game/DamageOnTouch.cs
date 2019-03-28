419a959b2231ca0a2efd3661e54d5f0f6008de79
Ryan <r.thomp09@gmail.com>
2019-03-28 16:05:49 -0700

    Fireball Explosions
    DamageOnTouch now supports a single particle system to play as an on hit effect.
    
    Fireball has a FireballExplosionEffect that plays when it hits something

--- Robots Rock/Assets/Scripts/Game/DamageOnTouch.cs
+++ Robots Rock/Assets/Scripts/Game/DamageOnTouch.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class DamageOnTouch : MonoBehaviour
 {
     public List<string> ignoreTags = new List<string>();
+    public ParticleSystem onHitEffect;
 
     public int damageAmount = 1;
     public bool disableOnHit = true;
+
+    private void Awake()
+    {
+        if (onHitEffect != null)
+        {
+            onHitEffect = Instantiate<ParticleSystem>(onHitEffect, transform.position, transform.rotation);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -27,6 +36,18 @@ public class DamageOnTouch : MonoBehaviour
             {
                 gameObject.SetActive(false);
             }
+        }
+
+        PlayOnHitEffect();
+    }
+
+    void PlayOnHitEffect()
+    {
+        if (onHitEffect != null)
+        {
+            onHitEffect.transform.position = transform.position;
+            onHitEffect.transform.rotation = transform.rotation;
+            onHitEffect.Play();
         }
     }
 }
