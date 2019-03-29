81a958d72bcad67658875de4b7c0371e0b1eefc9
Ryan <r.thomp09@gmail.com>
2019-03-28 19:12:20 -0700

    Health bar UI
    Health bar UI showing remaining health for the Robot

---
+++ Robots Rock/Assets/HUD.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text scoreText;
+    public Image[] healthBars;
+
+    private int currentHealthBarIndex;
+    public Color[] healthBarColors;
+
+    private void OnEnable()
+    {
+        currentHealthBarIndex = healthBars.Length - 1;
+        UpdateHealthBarColors();
+    }
+
+    public void RemoveHealth()
+    {
+        healthBars[currentHealthBarIndex].enabled = false;
+        currentHealthBarIndex = Mathf.Max(0, currentHealthBarIndex - 1);
+        UpdateHealthBarColors();
+    }
+
+    void UpdateHealthBarColors()
+    {
+        for (int i = 0; i < healthBars.Length; i++)
+        {
+            healthBars[i].color = healthBarColors[currentHealthBarIndex];
+        }
+    }
+
+    public void SetPlayerScore(int score)
+    {
+        scoreText.text = string.Format("SCORE:{0}", score);
+    }
+}
