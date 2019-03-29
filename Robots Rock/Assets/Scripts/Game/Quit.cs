4400fb125547a7fb23cca4d7f2b3e950a2adbc95
Ryan <r.thomp09@gmail.com>
2019-03-29 15:57:02 -0700

    Added ability to quit game to pause menu
    Pressing "Q" while paused now quits the game

---
+++ Robots Rock/Assets/Scripts/Game/Quit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+        }
+    }
+}
